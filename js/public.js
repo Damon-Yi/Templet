4788df4811a8385ec628574c0ad401925b0a12f9
Liyi <yi.li01@bqjr.cn>
2016-11-09 14:53:26 +0800

    loading  tips  break-line;  hammer下拉

--- js/public.js
+++ js/public.js
@@ -89,6 +89,18 @@ function commonTips(className,msg,time){
     },time*2);
 }
 
+/*提示，弹窗*/
+//tips('数据错误','tips_center',1500);
+//tips('数据错误','tips_left',1500);
+function tips(msg,className,time){
+    var tipsDiv = $('<div class="tips '+className+'"></div>');
+    $('body').append(tipsDiv);
+    tipsDiv.html(msg).addClass('tips_show');
+    setTimeout(function(){
+        tipsDiv.removeClass('tips_show').remove();
+    },time);
+}
+
 /*数字键盘*/
 function enterPsw($wrapper,parms){
 
@@ -224,6 +236,61 @@ function confirmWin($wrapper,parms){
         $confirmPanel.find('.a-info').text(parms.title);
         $confirmPanel.find('.a-btn-left').text(parms.leftBtnText);
         $confirmPanel.find('.a-btn-right').text(parms.rightBtnText);
+    }
+    me.init();
+    return me;
+}
+
+/*图片转Base64*/
+function getBase64Image(src) {
+    var img = document.createElement('img');
+    img.src = src;
+    console.log(img);
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    var dataURL = canvas.toDataURL("image/png");
+    console.log(dataURL);
+    //return dataURL
+    return dataURL.replace("data:image/png;base64,", "");
+} 
+
+/*loading*/
+var loading = function(param){
+    me = {}
+    me.param = param?param:{
+        'container':document.body,
+        'hasBg':true
+    };
+
+    me.init = function(){
+        me.loader=document.createElement("div"),
+        loader_bg=document.createElement("div"),
+        span_wrap=document.createElement("div");
+
+        me.loader.className="loader";
+        loader_bg.className="loader_bg";
+        span_wrap.className="span_wrap";
+        for(var i=0;i<5;i++){
+            var span=document.createElement("span");
+            span_wrap.appendChild(span);   
+        }
+        if(me.param.hasBg){
+           me.loader.appendChild(loader_bg); 
+        }
+        me.loader.appendChild(span_wrap); 
+        me.param.container.appendChild(me.loader);
+    }
+    me.show = function(){
+        me.loader.style.display = 'block';
+    }
+    me.hide = function(){
+        me.loader.style.display = 'none';
+    }
+    me.destroy = function(){
+        document.body.removeChild(me.loader);
     }
     me.init();
     return me;
